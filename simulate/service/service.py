29cfd589cea3bdb9a486626081245bca92e076b8
Anzhe Meng <meng@prepbaseballreport.com>
2025-02-18 10:12:03 -0500

    Triggered by queue messages

--- simulate/service/service.py
+++ simulate/service/service.py
@@ -1,5 +1,5 @@
-import logging, ast, requests, itertools
-from service.helper import delete_message, receive_message
+import logging, json, requests
+from service.helper import delete_message
 from time import time
 
 LOGGER = logging.getLogger(__name__)
@@ -14,61 +14,61 @@ def main(event, environment):
     :type environment: Dict[str, Any]
     """
     
-    LOGGER.info(event)
+    # LOGGER.info(event)
     QUEUE = environment['QUEUE']
 
-    messages = receive_message(QUEUE)
-    LOGGER.debug(f"Simulation Lambda receives {len(messages)} messages.")
+    record = event.get('Records')[0]
+    messages = json.loads(record.get('body'))
+    alpha_expressions, cookies = messages.get('messages'), requests.utils.cookiejar_from_dict(messages.get('cookies'))
 
     i = 0
-    for message, neutralization in itertools.product(messages, ["SUBINDUSTRY", "INDUSTRY", "SECTOR", "MARKET"]):
-        start = time()
-        try:
-            message_body = message['Body']
-            body = ast.literal_eval(message_body)
-            alpha_expression = body.get('expression')
-            cookies = requests.utils.cookiejar_from_dict(body.get('cookies'))
-            type_dict = {"type": "REGULAR"}
-            settings_dict = {
-                "instrumentType": "EQUITY",
-                "region": "USA",
-                "universe": "TOP3000",
-                "delay": 1,
-                "decay": 0,
-                "neutralization": neutralization,
-                "truncation": 0.08,
-                "pasteurization": "ON",
-                "unitHandling": "VERIFY",
-                "nanHandling": "ON",
-                "language": "FASTEXPR",
-                "visualization": False
-            }
-            regex_dict = {"regular": alpha_expression}
+    for alpha_expression in alpha_expressions:
+        for neutralization in ["SUBINDUSTRY", "INDUSTRY", "SECTOR", "MARKET"]:
+            start = time()
+            try:
+                type_dict = {"type": "REGULAR"}
+                settings_dict = {
+                    "instrumentType": "EQUITY",
+                    "region": "USA",
+                    "universe": "TOP3000",
+                    "delay": 1,
+                    "decay": 0,
+                    "neutralization": neutralization,
+                    "truncation": 0.08,
+                    "pasteurization": "ON",
+                    "unitHandling": "VERIFY",
+                    "nanHandling": "ON",
+                    "language": "FASTEXPR",
+                    "visualization": False
+                }
+                regex_dict = {"regular": alpha_expression}
 
-            simulation_data = {
-                **type_dict,
-                **settings_dict,
-                **regex_dict
-            }
+                simulation_data = {
+                    **type_dict,
+                    **settings_dict,
+                    **regex_dict
+                }
 
-            simulation_response = requests.post(
-                url='https://api.worldquantbrain.com/simulations',
-                json=simulation_data,
-                cookies=cookies
-            )
-            #TODO: get reauthenticated if the cookies expire
-            simulation_progress_url = simulation_response.headers.get("Location")
+                simulation_response = requests.post(
+                    url='https://api.worldquantbrain.com/simulations',
+                    json=simulation_data,
+                    cookies=cookies
+                )
+                #TODO: get reauthenticated if the cookies expire
+                simulation_progress_url = simulation_response.headers.get("Location")
 
-            while True:
-                simulation_progress_response = requests.get(simulation_progress_url, cookies=cookies)
-                retry_after_sec = float(simulation_progress_response.headers.get("Retry-After", 0))
-                if retry_after_sec == 0:
-                    break
+                while True:
+                    simulation_progress_response = requests.get(simulation_progress_url, cookies=cookies)
+                    retry_after_sec = float(simulation_progress_response.headers.get("Retry-After", 0))
+                    if retry_after_sec == 0:
+                        break
 
-            delete_message(QUEUE, message['ReceiptHandle'])
-        except Exception as e:
-            LOGGER.error(e)
-        finally:
-            end = time()
-            LOGGER.debug(f"Handling the {i+1}th message consumes {end - start} seconds.")
-            i += 1
+            except Exception as e:
+                LOGGER.error(e)
+            finally:
+                end = time()
+                LOGGER.info(f"Handling the {i+1}th payload consumes {round(end - start, 2)} seconds.")
+                i += 1
+
+    # delete messages
+    delete_message(QUEUE, record['receiptHandle'])
